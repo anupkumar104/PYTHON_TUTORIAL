532b690f9e1ddfc8744c1d70c7054695a7ec9f12
ANUP KUMAR <anupsaroj104@gmail.com>
2025-11-07 18:42:58 +0530

    Add files via upload

---
+++ basic_python/chapter1/module.py
@@ -0,0 +1,12 @@
+import pyjokes
+# this print a random jokes
+joke =pyjokes.get_joke()
+print(joke)
+# so thank 
+# that my program
+# another line
+"""
+this many line 
+comment
+
+"""
---
+++ basic_python/chapter1/problem3.py
@@ -0,0 +1,5 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("my name is anup and i am from BIET lucknow ")
+engine.runAndWait()
