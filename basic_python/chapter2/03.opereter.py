73b7ead9aedab13ea6242297703a8f9255520fb3
ANUP KUMAR <anupsaroj104@gmail.com>
2025-11-08 21:54:53 +0530

    Add files via upload

---
+++ basic_python/chapter2/03.opereter.py
@@ -0,0 +1,22 @@
+# arithmatic operaters 
+a=3
+b=6
+c=a+b
+print(c)
+#assignment operater
+a=4-2
+print(a)
+b=6
+b+=3
+c=2
+c-=3
+print(b)
+print(c)
+  
+#comparison operater
+d=5==4 
+print(d)
+
+# logical operater
+e= True or False
+print(e)
