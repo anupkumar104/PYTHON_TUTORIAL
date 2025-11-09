4763f8f72897d991c541ee0612602a8b14d2804e
ANUP KUMAR <anupsaroj104@gmail.com>
2025-11-09 12:00:38 +0530

    Add files via upload

---
+++ basic_python/chapter3/01_string.py
@@ -0,0 +1,19 @@
+# a='anup'
+# b="abhay"
+# c='''vivek'''
+# char=a[1]
+# char2=b[3]
+# a_short=a[0:3]
+# print(a)
+# print(b)
+# print(c)
+# print(char)
+# print(char2) 
+# print(a_short)   
+# # slicing of string
+name="anupsinghrawat"
+# sl1=name[0:4:2]
+# print(sl1)
+# negetive slicing of sting
+sl2=name[-4:-1]
+print(sl2)
---
+++ basic_python/chapter3/02_string_funtion.py
@@ -0,0 +1,9 @@
+name="anupsinghrawat"
+print(len(name))
+print(name.endswith("wat"))
+print(name.startswith("anu"))
+print(name.capitalize()) 
+print(name.count("a"))
+print(name.find("r"))
+print(name.replace("a","s"))
+
