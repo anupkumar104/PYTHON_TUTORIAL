af398d3355e2a7eff07e9829692a01ec04f19beb
ANUP KUMAR <anupsaroj104@gmail.com>
2025-11-09 11:59:09 +0530

    Add files via upload

---
+++ basic_python/chapter2/05.input.py
@@ -0,0 +1,6 @@
+# A=input("enter your name")
+# print("hey!",A)
+a=eval(input("enter the first number"))
+b=eval(input("enter the second number"))
+add=a+b
+print(add)
---
+++ basic_python/chapter2/06.problem.py
@@ -0,0 +1,4 @@
+# Write a python program to calculate the square of a number entered by the user
+a=int(input("enter the number"))
+s=a**2 #s=a*a
+print(" square of a number :",s)
