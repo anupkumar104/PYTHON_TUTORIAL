58ba722682bde713dcb2d2f8ee0db0b00f79946d
ANUP KUMAR <anupsaroj104@gmail.com>
2025-11-12 19:24:24 +0530

    Add files via upload

---
+++ basic_python/chapter5_ps/01_problem.py
@@ -0,0 +1,10 @@
+# Write a program to create a dictionary of Hindi words with values as their English
+# translation. Provide user with an option to look it up!
+dict={
+    "apple":"seb",
+    "ball":"gendh",
+    "cat":"billi",
+    "dog":"kutta"
+}
+w=input("enter your word :")
+print(dict[w])
