87280d7a23fd8d224f4dfd10588d89b434c5e3c8
ANUP KUMAR <anupsaroj104@gmail.com>
2025-11-27 20:36:28 +0530

    Add files via upload

---
+++ python_with_NLTK/p6.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+stopword=set(stopwords.words('english'))
+text="The leaves are falling from the trees and the boys were playing games while running and studies are continuing."
+tokenized_word=word_tokenize(text)
+tokenized_word_without_stopwords=[]
+for word in tokenized_word:
+    if word not in stopword:
+        tokenized_word_without_stopwords.append(word)
+#print("tokenized_word_without_stopwords :",tokenized_word_without_stopwords)
+stemmer=PorterStemmer()
+stemming_data=[]
+for word_1 in tokenized_word_without_stopwords:
+    stemming_data.append(stemmer.stem(word_1))
+#print(stemming_data)
+stemming_text=" ".join(stemming_data)
+print(stemming_text)
