2941a5cba125c8bdc2ae7cd51d1db8bcfef18e8a
ANUP KUMAR <anupsaroj104@gmail.com>
2025-12-05 21:49:20 +0530

    Add files via upload

---
+++ basic_python/py/p5.py
@@ -0,0 +1,9 @@
+a=eval(input("enter the value of a:"))
+b=eval(input("enter the value of b:"))
+c=eval(input("enter the value of c:"))
+if a>b and a>c:
+    print("a is grater")
+elif b>a and b>c:
+    print("b is grater")
+elif c>a and c>b:
+    print("c is grater")
