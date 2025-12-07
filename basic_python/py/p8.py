7b3dbf36c03c7f1d7e2fd4bfa986693b135725f3
ANUP KUMAR <anupsaroj104@gmail.com>
2025-12-07 12:41:52 +0530

    Add files via upload

---
+++ basic_python/py/p8.py
@@ -0,0 +1,7 @@
+a=eval(input("enter the value of a:"))
+b=eval(input("enter the value of b:"))
+print("1. add ,2. sub, 3.pro")
+ch= int (input("enter choices:"))
+match ch:
+    case 1:
+        print()
