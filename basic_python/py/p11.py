cc6b4b8b117c36edd46309cdc8b610a2daac455a
ANUP KUMAR <anupsaroj104@gmail.com>
2025-12-08 19:50:42 +0530

    Add files via upload

---
+++ basic_python/py/p11.py
@@ -0,0 +1,6 @@
+#write the program to impliment the concept of keyword argument
+def show (name ,age,address="xyz"):
+    print(name)
+    print(age)
+    print(address)
+show(name="anup",age=20,address="lucknow")
