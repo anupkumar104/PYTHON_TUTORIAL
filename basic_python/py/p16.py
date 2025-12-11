e2876b8433979f178451ce72f59c372bdb960c97
ANUP KUMAR <anupsaroj104@gmail.com>
2025-12-11 23:58:44 +0530

    Add files via upload

---
+++ basic_python/py/p16.py
@@ -0,0 +1,14 @@
+#write a program to intialize array with 5 element stored.
+# import numpy as np
+# x = np.array([12, 56, 23 ,85])
+# print(x)
+# print(max(x))
+# print(min(x))
+# print(sum(x))
+# print(x[2])
+# for i in x:
+#     print(x)
+# 10 element and only assign even number to array
+##write a program to intialize array with 5 element and print back largest and smallest element of array with its index or position s.
+import numpy as np
+x = np.array([12, 56, 23 ,85,89,78,8,80,6])
