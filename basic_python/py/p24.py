cc5e53e863f0327eae0ef4081af89f143b1ffed8
ANUP KUMAR <anupsaroj104@gmail.com>
2025-12-15 18:08:48 +0530

    Add files via upload

---
+++ basic_python/py/p24.py
@@ -0,0 +1,10 @@
+#list of dict
+import pandas as pd
+mydata=[
+    {"name":"sagar","age":25,"city":"lucknow"},
+    {"name":"amit","age":29,"city":"lucknow"},
+    {"name":"suman","age":35,"city":"lucknow"},
+    {"name":"nisha","age":24,"city":"lucknow"}]
+df=pd.DataFrame(mydata) 
+print(df)  
+    
